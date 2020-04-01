7926ae96cb6abadcfeda2359c99e097f9ff61714
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-01 03:11:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,10 @@
 def prime?(interger)
  if integers<2
   return false
-else
+ else
   (2.. ( integer - 1)).each.do |num|
   return false if integer % num==0
-end
-true
-end
-end
+ end
+ true
+ end
+ end
